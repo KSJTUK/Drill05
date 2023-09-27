c4878a4655b723e8697ca71c9bbd0ebb5268e2c5
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-09-27 17:51:13 +0900

    draw line 완성

--- line.py
+++ line.py
@@ -54,7 +54,17 @@ def draw_line(p1, p2):
     # fill here
     draw_big_point(p1)
     draw_big_point(p2)
-    pass
+    
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+
+    a = (y2 - y1) / (x2 - x1) # 기울기
+    b = y1 - a * x1
+    for x in range(x1, x2 + 1, 10):
+        y = a * x + b
+        draw_point((x, y))
+
+    draw_point(p2)
 
 
 prepare_turtle_canvas()
